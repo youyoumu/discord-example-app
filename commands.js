ed0eca762c1cb3b3f8649b4f701dd87ff4e21b17
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-06-12 14:57:29 +0700

    Prettify

--- commands.js
+++ commands.js
@@ -1,28 +1,28 @@
-import 'dotenv/config';
-import { getRPSChoices } from './game.js';
-import { capitalize, InstallGlobalCommands } from './utils.js';
+import 'dotenv/config'
+import { getRPSChoices } from './game.js'
+import { capitalize, InstallGlobalCommands } from './utils.js'
 
 // Get the game choices from game.js
 function createCommandChoices() {
-  const choices = getRPSChoices();
-  const commandChoices = [];
+  const choices = getRPSChoices()
+  const commandChoices = []
 
   for (let choice of choices) {
     commandChoices.push({
       name: capitalize(choice),
-      value: choice.toLowerCase(),
-    });
+      value: choice.toLowerCase()
+    })
   }
 
-  return commandChoices;
+  return commandChoices
 }
 
 // Simple test command
 const TEST_COMMAND = {
   name: 'test',
   description: 'Basic command',
-  type: 1,
-};
+  type: 1
+}
 
 // Command containing options
 const CHALLENGE_COMMAND = {
@@ -34,12 +34,12 @@ const CHALLENGE_COMMAND = {
       name: 'object',
       description: 'Pick your object',
       required: true,
-      choices: createCommandChoices(),
-    },
+      choices: createCommandChoices()
+    }
   ],
-  type: 1,
-};
+  type: 1
+}
 
-const ALL_COMMANDS = [TEST_COMMAND, CHALLENGE_COMMAND];
+const ALL_COMMANDS = [TEST_COMMAND, CHALLENGE_COMMAND]
 
-InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
+InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS)
